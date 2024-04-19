9e78c5cb7fef8b902d048d1b67d85b901686aa1d
Konf1ic <kenbui2809@gmail.com>
2024-04-19 14:25:41 +0700

    Min

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿public class MinValue
+{
+    public static int MinInArray(int[] array)
+    {
+        int min = array[0];
+        int index = 0;
+
+        for (int i = 1; i < array.Length; i++)
+        {
+            if (array[i] < min)
+            {
+                min = array[i];
+                index = i;
+            }
+        }
+
+        return index;
+    }
+
+    public static void Main(String[] args)
+    {
+        int[] arr = { 4, 12, 7, 8, 1, 6, 9 };
+        int index = MinInArray(arr);
+        Console.WriteLine("Vị trí của phần tử nhỏ nhất trong mảng là: " + index);
+    }
+}
